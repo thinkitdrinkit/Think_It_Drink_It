7bbfa0b763c0da201df3bbdc822d9cd3cdc33c28
milolang <milolang@gmail.com>
2013-10-30 12:29:29 -0400

    added ThinkitDrinkitapi

---
+++ App5/ThinkitDrinkitApi/Controllers/AdminController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using ThinkitDrinkitApi.Models;
+
+namespace ThinkitDrinkitApi.Controllers
+{
+    public class AdminController : ApiController
+    {
+        private OrdersContext db = new OrdersContext();
+
+        // GET api/Admin
+        public IEnumerable<Product> GetProducts()
+        {
+            return db.Products.AsEnumerable();
+        }
+
+        // GET api/Admin/5
+        public Product GetProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return product;
+        }
+
+        // PUT api/Admin/5
+        public HttpResponseMessage PutProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != product.Id)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/Admin
+        public HttpResponseMessage PostProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, product);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = product.Id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Admin/5
+        public HttpResponseMessage DeleteProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Products.Remove(product);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ App5/ThinkitDrinkitApi/Models/OrdersContextInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThinkitDrinkitApi.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    
+    
+    //Milo: This feature is great for testing, but don’t use the DropCreateDatabaseIfModelChanges class in production,, because you could lose your data if someone changes a model class. 
+    public class OrdersContextInitializer : DropCreateDatabaseIfModelChanges<OrdersContext>
+    {
+        protected override void Seed(OrdersContext context)
+        {
+            var products = new List<Product>()            
+            {
+                new Product() { Name = "Tomato Soup", Price = 1.39M, ActualCost = .99M },
+                new Product() { Name = "Hammer", Price = 16.99M, ActualCost = 10 },
+                new Product() { Name = "Yo yo", Price = 6.99M, ActualCost = 2.05M }
+            };
+
+            products.ForEach(p => context.Products.Add(p));
+            context.SaveChanges();
+
+            var order = new Order() { Customer = "Bob" };
+            var od = new List<OrderDetail>()
+            {
+                new OrderDetail() { Product = products[0], Quantity = 2, Order = order},
+                new OrderDetail() { Product = products[1], Quantity = 4, Order = order }
+            };
+            context.Orders.Add(order);
+            od.ForEach(o => context.OrderDetails.Add(o));
+
+            context.SaveChanges();
+        }
+    }
+}
